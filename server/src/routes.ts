67bc803fac53233ee0dbc307f0ac8d1331f80883
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-06-14 19:50:57 -0400

    celebrate input verification

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
 
 import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
+import { string } from '@hapi/joi';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -15,6 +18,23 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      phone: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      state: string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create);
 
 export default routes;
