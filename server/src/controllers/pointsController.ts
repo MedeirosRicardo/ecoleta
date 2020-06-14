2dc140c953f78cf4d4982dcc57c8b521c3fd4076
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-06-14 14:15:10 -0400

    backend file upload done

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -52,7 +52,7 @@ class PointsController {
 		const trx = await knex.transaction();
 
 		const point = {
-			image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+			image: request.file.filename,
 			name,
 			email,
 			phone,
@@ -66,8 +66,11 @@ class PointsController {
 
 		const point_id = insertedIds[0];
 
-		const pointItems = items.map((item_id: number) => {
-			return {
+		const pointItems = items
+			.split(',')
+			.map((item: string) => Number(item.trim()))
+			.map((item_id: number) => {
+				return {
 				item_id,
 				point_id
 			};
