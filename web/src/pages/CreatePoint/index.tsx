e7a225f8c2d586429222eb86e6d6f33a281c66d8
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-06-14 19:34:23 -0400

    web image upload

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -4,6 +4,7 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
+import Dropzone from '../../components/Dropzone';
 
 import api from '../../services/api';
 import './styles.css';
@@ -51,6 +52,7 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -137,16 +139,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      phone,
-      state,
-      city,
-      latitude,
-      longitude,
-      items
-    }
+    const data = new FormData();
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('phone', phone);
+      data.append('state', state);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+      
+      if (selectedFile) {
+        data.append('image', selectedFile);
+      }
 
     await api.post('points', data);
 
@@ -168,6 +174,8 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Register a Point of Collection</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
